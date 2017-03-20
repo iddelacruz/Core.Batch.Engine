af73d9a61a2737104bc01434683340059f1f0ae2
Israel de la Cruz <banense@gmail.com>
2017-03-20 20:23:22 +0100

    Added section config

--- Core.Batch.Engine/Core.Batch.Engine/Base/OperationResponseMessage.cs
+++ Core.Batch.Engine/Core.Batch.Engine/Base/OperationResponseMessage.cs
@@ -2,10 +2,19 @@
 
 namespace Core.Batch.Engine.Base
 {
+    /// <summary>
+    /// Value type to manage the operation responses.
+    /// </summary>
     public struct OperationResponseMessage
     {
+        /// <summary>
+        /// The operation status.
+        /// </summary>
         public OperationStatus Status { get; set; }
 
+        /// <summary>
+        /// Self-rated property that verify if the operation was Ok.
+        /// </summary>
         public bool IsSuccessStatusCode
         {
             get
